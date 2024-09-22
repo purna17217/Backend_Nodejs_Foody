2ceb40807425160cae654a664950d320214fa081
purna17217 <tirunagaripurnachandrarao@gmail.com>
2024-09-22 18:43:08 +0530

    Added Path Module

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const bodyparser = require('body-parser');
 const firmRoutes = require('./routes/firmRoutes');
 const productRoutes = require('./routes/productRoutes');
 const path = require('path');
-
+const cors = require('cors');
 
 
 const app = express()
@@ -17,6 +17,7 @@ mongoose.connect(process.env.MONGO_URI)
 .then(()=>console.log("mongoose connected"))
 .catch((error)=>console.log(error))
 
+app.use(cors())
 app.use(bodyparser.json());
 app.use('/vendor',vendorRoutes);
 app.use('/firm',firmRoutes);
@@ -25,6 +26,10 @@ app.use('/uploads',express.static('uploads'));
 app.listen(PORT,()=>{
     console.log(`server started at ${PORT}`);
 })
-app.use('/',(req,res)=>{
-    res.send("<h1>Hello Foody")
-})
+// app.use('/',(req,res)=>{
+//     res.send("Hello Foody")
+// })
+app.use('/', (req, res) => {
+    res.status(404).json({ error: 'Route not found' });
+  });
+  
